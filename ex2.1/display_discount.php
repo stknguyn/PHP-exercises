414ea22772e1004609ea697ffc8b77c45ba79685
Stork Nguyen <storknguyen.286@gmail.com>
2023-09-07 20:22:49 +0700

    Update display_discount.php

--- ex2.1/display_discount.php
+++ ex2.1/display_discount.php
@@ -1,11 +1,11 @@
 <?php
-$product_description = filter_input(INPUT_POST, 'product_decription');
+$product_description = filter_input(INPUT_POST, 'product_description');
 $list_price = filter_input(INPUT_POST, 'list_price');
 $discount_percent = filter_input(INPUT_POST, 'discount_percent');
-
+//calculating
 $discount = $list_price * $discount_percent * 0.01;
 $discount_price = $list_price - $discount;
-
+//formating
 $list_price_f = "$" . number_format($list_price, 2);
 $discount_percent_f = number_format($discount_percent, 2) . "%";
 $discount_f = "$" . number_format($discount, 2);
@@ -25,8 +25,8 @@ $discount_price_f = "$" . number_format($discount_price, 2);
     <h1>Product Discount Calculator</h1>
 
     <label>Product Description:</label>
-    <span><?php echo htmlspecialchars($product_description); ?></span><br>
-
+    <span><?php echo htmlspecialchars($product_description); ?></span>
+    <br>
     <label>List Price:</label>
     <span><?php echo htmlspecialchars($list_price_f); ?></span><br>
 
