19f0c88e7b830f7d69632f7439689566faf672b7
Stork Nguyen <storknguyen.286@gmail.com>
2023-09-18 20:48:09 +0700

    update bootstrap

--- medicine/index.php
+++ medicine/index.php
@@ -1,34 +1,48 @@
 <?php require_once('database.php');
-$queryCategory = 'SELECT * FROM medicine';
-$statements = $db -> prepare($queryCategory);
-$statements -> execute();
-$medicines= $statements -> fetchAll();
-$medicineTypeName = $medicines['medicineTypeName'];
-$statements -> closeCursor();
+$query = 'SELECT * FROM medicine';
+$statements = $db->prepare($query);
+$statements->execute();
+$medicines = $statements->fetchAll();
+$statements->closeCursor();
+
 ?>
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Medical Care</title>
+  <title>Document</title>
 </head>
+
 <body>
-  <header>
-    <h1>Medicine Manager</h1>
-  </header>
-  <main>
-    <h1>List</h1>
-    <aside>
-      <h2>Types</h2>
-      <nav>
-        <ul>
-          <?php foreach ($medicines as $medicine) :?>
-            <li> <?php echo $medicines['medicineTypeName']?></li>
-            <?php endforeach;?>
-        </ul>
-      </nav>
-    </aside>
-  </main>
+  <table border="1" class="table table-striped table-hover">
+    <thead>
+      <th scope="col">medicineID</th>
+      <th scope="col">medicineTypeID</th>
+      <th scope="col">quantity</th>
+      <th scope="col">decription</th>
+    </thead>
+    <tbody>
+    <?php foreach ($medicines as $medicine) : ?>
+      <tr>
+        <th scope="row">
+          <?php echo $medicine['medicineID'] ?>
+        </th>
+        <td>
+          <?php echo $medicine['medicineTypeID'] ?>
+        </td>
+        <td>
+          <?php echo $medicine['quantity'] ?>
+        </td>
+        <td>
+          <?php echo $medicine['decription'] ?>
+        </td>
+      </tr>
+      <?php endforeach; ?>
+    </tbody>
+  </table>
 </body>
+
 </html>
