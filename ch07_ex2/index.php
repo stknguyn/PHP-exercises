b39474209ea0a9802317b1dccf71cb7db9c917af
Stork Nguyen <storknguyen.286@gmail.com>
2023-09-29 17:29:56 +0700

    update ch7_ex2

--- ch07_ex2/index.php
+++ ch07_ex2/index.php
@@ -1,44 +1,56 @@
-<?php 
-    //set default value of variables for initial page load
-    if (!isset($investment)) { $investment = '10000'; } 
-    if (!isset($interest_rate)) { $interest_rate = '5'; } 
-    if (!isset($years)) { $years = '5'; } 
+<?php
+//set default value of variables for initial page load
+if (!isset($investment)) {
+  $investment = '10000';
+}
+if (!isset($interest_rate)) {
+  $interest_rate = '5';
+}
+if (!isset($years)) {
+  $years = '5';
+}
 ?>
 <!DOCTYPE html>
 <html>
+
 <head>
-    <title>Future Value Calculator</title>
-    <link rel="stylesheet" type="text/css" href="main.css"/>
+  <title>Future Value Calculator</title>
+  <link rel="stylesheet" type="text/css" href="main.css" />
 </head>
 
 <body>
-    <main>
+  <main>
     <h1>Future Value Calculator</h1>
     <?php if (!empty($error_message)) { ?>
-        <p class="error"><?php echo $error_message; ?></p>
-    <?php } // end if ?>
+      <p class="error"><?php echo $error_message; ?></p>
+    <?php } // end if 
+    ?>
     <form action="display_results.php" method="post">
 
-        <div id="data">
-            <label>Investment Amount:</label>
-            <input type="text" name="investment"
-                   value="<?php echo $investment; ?>"/><br>
+      <div id="data">
+        <label>Investment Amount:</label>
+        <select name="investment" style="width: 200px; height: 40px;">
+          <?php for ($i = (int) $investment; $i <= 50000; $i += 5000) : ?>
+            <option value="<?php echo $i ?>"><?php echo $i ?></option>
+          <?php endfor; ?>
+        </select>
+        <label>Yearly Interest Rate:</label>
+        <select name="interest_rate" style="width: 200px; height: 40px;">
+          <?php for ($x = (float)$interest_rate; $x <= 12; $x += 0.5) : ?>
+            <option value="<?php echo $x ?>"><?php echo $x ?></option>
+          <?php endfor; ?>
+        </select>
+        <label>Number of Years:</label>
+        <input style="width: 180px;height:40px;" type="text" name="years" value="<?php echo $years; ?>" /><br>
+      </div>
 
-            <label>Yearly Interest Rate:</label>
-            <input type="text" name="interest_rate"
-                   value="<?php echo $interest_rate; ?>"/><br>
-
-            <label>Number of Years:</label>
-            <input type="text" name="years"
-                   value="<?php echo $years; ?>"/><br>
-        </div>
-
-        <div id="buttons">
-            <label>&nbsp;</label>
-            <input type="submit" value="Calculate"/><br>
-        </div>
+      <div id="buttons">
+        <label>&nbsp;</label>
+        <input type="submit" value="Calculate" /><br>
+      </div>
 
     </form>
-    </main>
+  </main>
 </body>
+
 </html>
