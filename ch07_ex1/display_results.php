581595ed2fee50800d7989c813621110e01d75d9
Stork Nguyen <storknguyen.286@gmail.com>
2023-09-29 17:05:16 +0700

    formatted display_result.php

--- ch07_ex1/display_results.php
+++ ch07_ex1/display_results.php
@@ -3,12 +3,12 @@ $email = filter_input(INPUT_POST, 'email', FILTER_VALIDATE_EMAIL);
 $password = filter_input(INPUT_POST, 'password');
 $phone = filter_input(INPUT_POST, 'phone',);
 $heard_from = filter_input(INPUT_POST, 'heard_from');
-$wants_updates = filter_input(INPUT_POST,'wants_updates');
+$wants_updates = filter_input(INPUT_POST, 'wants_updates');
 $check_wants_updates = isset($wants_updates);
 $contact_via = filter_input(INPUT_POST, 'contact_via');
 $comments = filter_input(INPUT_POST, 'comments');
 // get the rest of the data for the form
-if($heard_from == NULL) {
+if ($heard_from == NULL) {
   $heard_from = 'unknown';
 }
 
@@ -34,22 +34,23 @@ if($heard_from == NULL) {
     <span><?php echo htmlspecialchars($email); ?></span><br>
 
     <label>Password:</label>
-    <span><?php echo htmlspecialchars($password)?></span><br>
+    <span><?php echo htmlspecialchars($password) ?></span><br>
 
     <label>Phone Number:</label>
     <span><?php echo $phone ?></span><br>
 
     <label>Heard From:</label>
-    <span><?php echo $heard_from?></span><br>
+    <span><?php echo $heard_from ?></span><br>
 
     <label>Send Updates:</label>
-    <span><?php if($check_wants_updates != NULL || $check_wants_updates != FALSE) echo 'Yes'; else echo 'No' ?></span><br>
+    <span><?php if ($check_wants_updates != NULL || $check_wants_updates != FALSE) echo 'Yes';
+          else echo 'No' ?></span><br>
 
     <label>Contact Via:</label>
-    <span><?php echo $contact_via?></span><br><br>
+    <span><?php echo $contact_via ?></span><br><br>
 
     <span>Comments:</span><br>
-    <span><?php echo htmlspecialchars($comments);?></span><br>
+    <span><?php echo htmlspecialchars($comments); ?></span><br>
   </main>
 </body>
 
