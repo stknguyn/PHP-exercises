1a9e62bd5656021ec6b7df24439bf3da70e3f71f
Stork Nguyen <storknguyen.286@gmail.com>
2023-09-29 17:04:10 +0700

    update ch7_ex1

--- ch07_ex1/display_results.php
+++ ch07_ex1/display_results.php
@@ -1,45 +1,56 @@
 <?php
-    // get the data from the form
-    $email = filter_input(INPUT_POST, 'email', FILTER_VALIDATE_EMAIL);
+$email = filter_input(INPUT_POST, 'email', FILTER_VALIDATE_EMAIL);
+$password = filter_input(INPUT_POST, 'password');
+$phone = filter_input(INPUT_POST, 'phone',);
+$heard_from = filter_input(INPUT_POST, 'heard_from');
+$wants_updates = filter_input(INPUT_POST,'wants_updates');
+$check_wants_updates = isset($wants_updates);
+$contact_via = filter_input(INPUT_POST, 'contact_via');
+$comments = filter_input(INPUT_POST, 'comments');
+// get the rest of the data for the form
+if($heard_from == NULL) {
+  $heard_from = 'unknown';
+}
 
-    // get the rest of the data for the form
+// for the heard_from radio buttons,
+// display a value of 'Unknown' if the user doesn't select a radio button
 
-    // for the heard_from radio buttons,
-    // display a value of 'Unknown' if the user doesn't select a radio button
-
-    // for the wants_updates check box,
-    // display a value of 'Yes' or 'No'
+// for the wants_updates check box,
+// display a value of 'Yes' or 'No'
 ?>
 <!DOCTYPE html>
 <html>
+
 <head>
-    <title>Account Information</title>
-    <link rel="stylesheet" type="text/css" href="main.css"/>
+  <title>Account Information</title>
+  <link rel="stylesheet" type="text/css" href="main.css" />
 </head>
+
 <body>
-    <main>
-        <h1>Account Information</h1>
+  <main>
+    <h1>Account Information</h1>
 
-        <label>Email Address:</label>
-        <span><?php echo htmlspecialchars($email); ?></span><br>
+    <label>Email Address:</label>
+    <span><?php echo htmlspecialchars($email); ?></span><br>
 
-        <label>Password:</label>
-        <span><!-- add PHP code here--></span><br>
+    <label>Password:</label>
+    <span><?php echo htmlspecialchars($password)?></span><br>
 
-        <label>Phone Number:</label>
-        <span></span><br>
+    <label>Phone Number:</label>
+    <span><?php echo $phone ?></span><br>
 
-        <label>Heard From:</label>
-        <span></span><br>
+    <label>Heard From:</label>
+    <span><?php echo $heard_from?></span><br>
 
-        <label>Send Updates:</label>
-        <span></span><br>
+    <label>Send Updates:</label>
+    <span><?php if($check_wants_updates != NULL || $check_wants_updates != FALSE) echo 'Yes'; else echo 'No' ?></span><br>
 
-        <label>Contact Via:</label>
-        <span></span><br><br>
+    <label>Contact Via:</label>
+    <span><?php echo $contact_via?></span><br><br>
 
-        <span>Comments:</span><br>
-        <span></span><br>        
-    </main>
+    <span>Comments:</span><br>
+    <span><?php echo htmlspecialchars($comments);?></span><br>
+  </main>
 </body>
+
 </html>
--- ch07_ex1/index.php
+++ ch07_ex1/index.php
@@ -1,17 +1,18 @@
 <!DOCTYPE html>
 <html>
+
 <head>
-    <title>Account Sign Up</title>
-    <link rel="stylesheet" type="text/css" href="main.css"/>
+  <title>Account Sign Up</title>
+  <link rel="stylesheet" type="text/css" href="main.css" />
 </head>
 
 <body>
-    <main>
+  <main>
     <h1>Account Sign Up</h1>
     <form action="display_results.php" method="post">
 
-    <fieldset> 
-    <legend>Account Information</legend>
+      <fieldset>
+        <legend>Account Information</legend>
         <label>E-Mail:</label>
         <input type="text" name="email" value="" class="textbox">
         <br>
@@ -22,10 +23,10 @@
 
         <label>Phone Number:</label>
         <input type="text" name="phone" value="" class="textbox">
-    </fieldset>
+      </fieldset>
 
-    <fieldset>
-    <legend>Settings</legend>
+      <fieldset>
+        <legend>Settings</legend>
 
         <p>How did you hear about us?</p>
         <input type="radio" name="heard_from" value="Search Engine">
@@ -36,25 +37,26 @@
         Other<br>
 
         <p>Would you like to receive announcements about new products
-           and special offers?</p>
+          and special offers?</p>
         <input type="checkbox" name="wants_updates">YES, I'd like to receive
         information about new products and special offers.<br>
 
         <p>Contact via:</p>
         <select name="contact_via">
-                <option value="email">Email</option>
-                <option value="text">Text Message</option>
-                <option value="phone">Phone</option>
+          <option value="email">Email</option>
+          <option value="text">Text Message</option>
+          <option value="phone">Phone</option>
         </select>
 
         <p>Comments:</p>
         <textarea name="comments" rows="4" cols="50"></textarea>
-    </fieldset>
+      </fieldset>
 
-    <input type="submit" value="Submit">
-    <br>
+      <input type="submit" value="Submit">
+      <br>
 
     </form>
-    </main>
+  </main>
 </body>
+
 </html>
