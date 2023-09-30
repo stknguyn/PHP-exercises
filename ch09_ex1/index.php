db3770527d5d5a9b422b61aa34dd1c1e40e0c46b
Stork Nguyen <storknguyen.286@gmail.com>
2023-09-30 16:04:22 +0700

    update ch9_ex1

--- ch09_ex1/index.php
+++ ch09_ex1/index.php
@@ -9,36 +9,76 @@ $message = 'Enter some data and click on the Submit button.';
 $action = filter_input(INPUT_POST, 'action');
 
 switch ($action) {
-    case 'process_data':
-        $name = filter_input(INPUT_POST, 'name');
-        $email = filter_input(INPUT_POST, 'email');
-        $phone = filter_input(INPUT_POST, 'phone');
+  case 'process_data':
+    $name = filter_input(INPUT_POST, 'name');
+    $email = filter_input(INPUT_POST, 'email');
+    $phone = filter_input(INPUT_POST, 'phone');
 
-        /*************************************************
-         * validate and process the name
-         ************************************************/
-        // 1. make sure the user enters a name
-        // 2. display the name with only the first letter capitalized
+    if ($name == null || $email == null || $phone == null) {
+      $message = 'Please enter full information!';
+    }
+    /*************************************************
+     * validate and process the name
+     ************************************************/
+    // 1. make sure the user enters a name
+    // 2. display the name with only the first letter capitalized
+    trim($name);
+    trim($email);
+    trim($phone);
 
-        /*************************************************
-         * validate and process the email address
-         ************************************************/
-        // 1. make sure the user enters an email
-        // 2. make sure the email address has at least one @ sign and one dot character
+    $name = strtolower($name);
+    $name = ucwords($name);
+    $space_index = strpos($name, ' ');
+    if ($space_index === NULL || $space_index === FALSE) {
+      $main_name = $name;
+    } else {
+      $main_name = substr($name, 0, $space_index);
+    }
+    /*************************************************
+     * validate and process the email address
+     ************************************************/
+    // 1. make sure the user enters an email
+    // 2. make sure the email address has at least one @ sign and one dot character
+    if (strpos($email, '@') === FALSE) {
+      $message = 'Email must contain @';
+      break;
+    } else if (strpos($email, '.') === FALSE) {
+      $message = 'Email must contain dot (.)';
+      break;
+    }
 
-        /*************************************************
-         * validate and process the phone number
-         ************************************************/
-        // 1. make sure the user enters at least seven digits, not including formatting characters
-        // 2. format the phone number like this 123-4567 or this 123-456-7890
-
-        /*************************************************
-         * Display the validation message
-         ************************************************/
-        $message = "This page is under construction.\n" .
-                   "Please write the code that processes the data.";
-
-        break;
+    /*************************************************
+     * validate and process the phone number
+     ************************************************/
+    // 1. make sure the user enters at least seven digits, not including formatting characters
+    // 2. format the phone number like this 123-4567 or this 123-456-7890
+    $phone = str_replace('-', '', $phone);
+    $phone = str_replace('(', '', $phone);
+    $phone = str_replace(')', '', $phone);
+    $phone = str_replace('.', '', $phone);
+    $phone = str_replace(' ', '', $phone);
+    if (strlen($phone) < 7) {
+      $message = 'Phone number must contain at least 7 digits.';
+      break;
+    } else if (strlen($phone) == 7) {
+      $part1 = substr($phone, 0, 3);
+      $part2 = substr($phone, 3);
+      $phone = $part1 . '-' . $part2;
+    } else {
+      $part1 = substr($phone, 0, 3);
+      $part2 = substr($phone, 3, 3);
+      $part3 = substr($phone, 6);
+      $phone = $part1 . '-' . $part2 . '-' . $part3;
+    }
+    /*************************************************
+     * Display the validation message
+     ************************************************/
+    $message = "
+    Hello $main_name.\n" .
+      "Thank you for entering this data:\n" .
+      "Name: $name\n" .
+      "Email: $email\n" .
+      "Phone: $phone\n";
+    break;
 }
 include 'string_tester.php';
-?>
--- ch09_ex1/string_tester.php
+++ ch09_ex1/string_tester.php
@@ -1,40 +1,39 @@
 <!DOCTYPE html>
 <html>
+
 <head>
-    <title>String Tester</title>
-    <link rel="stylesheet" type="text/css" href="main.css">
+  <title>String Tester</title>
+  <link rel="stylesheet" type="text/css" href="main.css">
 </head>
 
 <body>
-    <main>
-        <h1>String Tester</h1>
-        <form action="." method="post">
-        <input type="hidden" name="action" value="process_data">
+  <main>
+    <h1>String Tester</h1>
+    <form action="." method="post">
+      <input type="hidden" name="action" value="process_data">
 
-        <label>Name:</label>
-        <input type="text" name="name" 
-               value="<?php echo htmlspecialchars($name); ?>">
-        <br>
+      <label>Name:</label>
+      <input type="text" name="name" value="<?php echo htmlspecialchars($name); ?>">
+      <br>
 
-        <label>E-Mail:</label>
-        <input type="text" name="email" 
-               value="<?php echo htmlspecialchars($email); ?>">
-        <br>
+      <label>E-Mail:</label>
+      <input type="text" name="email" value="<?php echo htmlspecialchars($email); ?>">
+      <br>
 
-        <label>Phone Number:</label>
-        <input type="text" name="phone" 
-               value="<?php echo htmlspecialchars($phone); ?>">
-        <br>
+      <label>Phone Number:</label>
+      <input type="text" name="phone" value="<?php echo htmlspecialchars($phone); ?>">
+      <br>
 
-        <label>&nbsp;</label>
-        <input type="submit" value="Submit">
-        <br>
+      <label>&nbsp;</label>
+      <input type="submit" value="Submit">
+      <br>
 
-        </form>
+    </form>
 
-        <h2>Message:</h2>
-        <p><?php echo nl2br(htmlspecialchars($message)); ?></p>
+    <h2>Message:</h2>
+    <p style="font-family: monospace; font-weight: bold;"><?php echo nl2br(htmlspecialchars($message)); ?></p>
 
-    </main>
+  </main>
 </body>
+
 </html>
