d2f051b3a6b5031aa422b40e478e14230130e8b5
Stork Nguyen <storknguyen.286@gmail.com>
2023-09-30 14:39:27 +0700

    update ch8_ex1

--- ch08_ex1/index.php
+++ ch08_ex1/index.php
@@ -1,26 +1,31 @@
 <?php
 
-$customer_type = filter_input(INPUT_POST, 'type');
+$customer_type_pre = filter_input(INPUT_POST, 'type');
+$customer_type = strtoupper($customer_type_pre);
 $invoice_subtotal = filter_input(INPUT_POST, 'subtotal');
-
-if ($customer_type == 'R') {
-    if ($invoice_subtotal < 100) {
-        $discount_percent = .0;
-    } else if ($invoice_subtotal >= 100 && $invoice_subtotal < 250) {
-        $discount_percent = .1;
-    } else if ($invoice_subtotal >= 250) {
-        $discount_percent = .25;
+$discount_percent = 0;
+switch ($customer_type) {
+  case 'R':
+    if ($invoice_subtotal >= 250 && $invoice_subtotal < 500) {
+      $discount_percent = .25;
+    } else if ($invoice_subtotal >= 500) {
+      $discount_percent = 0.3;
     }
-} else if ($customer_type == 'C') {
-    if ($invoice_subtotal < 250) {
-        $discount_percent = .2;
-    } else {
-        $discount_percent = .3;
+    break;
+  case 'C':
+    $discount_percent = 0.2;
+    break;
+  case 'T':
+    if ($invoice_subtotal < 500) {
+      $discount_percent = 0.4;
+    } else if ($invoice_subtotal >= 500) {
+      $discount_percent = 0.5;
     }
-} else {
-    $discount_percent = .0;
+    break;
+  default:
+    $discount_percent = 0.1;
+    break;
 }
-
 $discount_amount = $invoice_subtotal * $discount_percent;
 $invoice_total = $invoice_subtotal - $discount_amount;
 
@@ -29,5 +34,3 @@ $discount = number_format($discount_amount, 2);
 $total = number_format($invoice_total, 2);
 
 include 'invoice_total.php';
-
-?>
