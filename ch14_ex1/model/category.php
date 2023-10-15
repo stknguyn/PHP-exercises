83231e4702775e3f606e54c4c83248f5140f8e1f
Stork Nguyen <storknguyen.286@gmail.com>
2023-10-15 21:47:02 +0700

    update ch14_ex1

--- ch14_ex1/model/category.php
+++ ch14_ex1/model/category.php
@@ -1,27 +1,32 @@
 <?php
-class Category {
-    private $id;
-    private $name;
+class Category
+{
+  private $id;
+  private $name;
 
-    public function __construct($id, $name) {
-        $this->id = $id;
-        $this->name = $name;
-    }
+  public function __construct()
+  {
+    $this->id = null;
+    $this->name = '';
+  }
 
-    public function getID() {
-        return $this->id;
-    }
+  public function getID()
+  {
+    return $this->id;
+  }
 
-    public function setID($value) {
-        $this->id = $value;
-    }
+  public function setID($value)
+  {
+    $this->id = $value;
+  }
 
-    public function getName() {
-        return $this->name;
-    }
+  public function getName()
+  {
+    return $this->name;
+  }
 
-    public function setName($value) {
-        $this->name = $value;
-    }
+  public function setName($value)
+  {
+    $this->name = $value;
+  }
 }
-?>
