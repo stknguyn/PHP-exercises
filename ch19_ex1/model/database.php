22ab9e8ea0038fa54528510353df89a173775fb4
Stork Nguyen <storknguyen.286@gmail.com>
2023-10-28 14:21:51 +0700

    update database to class

--- ch19_ex1/model/database.php
+++ ch19_ex1/model/database.php
@@ -1,19 +1,32 @@
 <?php
-$dsn = 'mysql:host=localhost;dbname=my_guitar_shop2';
-$username = 'root';
-$password = '';
-$options = array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION);
+class Database
+{
+  private static $dsn = 'mysql:host=localhost;dbname=my_guitar_shop2';
+  private static $username = 'root';
+  private static $password = '';
+  private static $options = array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION);
+  private static $db;
+  public function __construct()
+  {
+  }
+  public static function getDB()
+  {
+    if (!isset(self::$db)) {
+      try {
+        self::$db = new PDO(self::$dsn, self::$username, self::$password, self::$options);
+      } catch (PDOException $e) {
+        $error_message = $e->getMessage();
+        include 'errors/db_error_connect.php';
+        exit;
+      }
+    }
+    return self::$db;
+  }
 
-try {
-    $db = new PDO($dsn, $username, $password, $options);
-} catch (PDOException $e) {
-    $error_message = $e->getMessage();
-    include 'errors/db_error_connect.php';
-    exit;
-}
 
-function display_db_error($error_message) {
+  public static function display_db_error($error_message)
+  {
     include 'errors/db_error.php';
     exit;
+  }
 }
-?>
