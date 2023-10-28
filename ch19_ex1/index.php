8df26f1074238faea1ad6cd432e6decafd3adf00
Stork Nguyen <storknguyen.286@gmail.com>
2023-10-28 15:05:15 +0700

    update ch19_ex1

--- ch19_ex1/index.php
+++ ch19_ex1/index.php
@@ -7,38 +7,45 @@ require('model/product_db.php');
 /*********************************************
  * Select some products
  **********************************************/
-
+$database = new Database();
+$productDB = new ProductDB();
 // Sample data
-$cat_id = 2;
+$cat_id = 1;
 
 // Get the products
-$products = get_products_by_category($cat_id);
+$products = $productDB->get_products_by_category($cat_id);
 
 /***************************************
  * Delete a product
  ****************************************/
 
 // Sample data
-$product_name = 'Fender Telecaster';
+$product_name = 'Fender Telecaster 1';
 
-// Delete the product and display an appropriate messge
-$delete_message = "No rows were deleted.";
-
+$product_id = $productDB->get_product_id_by_name($product_name);
+$result = $productDB->delete_product($product_id);
+if ($result > 0) {
+  $delete_message = "$result row has been deleted";
+} else {
+  $delete_message = "No rows were deleted.";
+}
 /***************************************
  * Insert a product
  ****************************************/
 
 // Sample data
 $category_id = 1;
-$code = 'tele';
-$name = 'Fender Telecaster';
+$code = 'quan';
+$name = 'Fender Telecaster 1';
 $description = 'NA';
 $price = '949.99';
 
 // Insert the data
-
-// Display an appropriate message
-$insert_message = "No rows were inserted.";
+$insert_result = $productDB->add_product($cat_id, $code, $name, $description, $price, 0);
+if ($insert_result > 0) {
+  $insert_message = "1 row has been inserted with ID: $insert_result";
+} else {
+  $insert_message = "No rows were inserted.";
+}
 
 include 'home.php';
-?>
--- ch19_ex1/model/product_db.php
+++ ch19_ex1/model/product_db.php
@@ -1,43 +1,53 @@
 <?php
-function get_products_by_category($category_id) {
-    global $db;
+class ProductDB
+{
+  function get_products_by_category($category_id)
+  {
+    $db = Database::getDB();
     $query = 'SELECT * FROM products
               WHERE categoryID = :category_id
               ORDER BY productID';
     try {
-        $statement = $db->prepare($query);
-        $statement->bindValue(':category_id', $category_id);
-        $statement->execute();
-        $result = $statement->fetchAll();
-        $statement->closeCursor();
-        return $result;
+      $statement = $db->prepare($query);
+      $statement->bindValue(':category_id', $category_id);
+      $statement->execute();
+      $result = $statement->fetchAll();
+      $statement->closeCursor();
+      return $result;
     } catch (PDOException $e) {
-        $error_message = $e->getMessage();
-        display_db_error($error_message);
+      $error_message = $e->getMessage();
+      Database::display_db_error($error_message);
     }
-}
+  }
 
-function get_product($product_id) {
-    global $db;
+  function get_product($product_id)
+  {
+    $db = Database::getDB();
     $query = 'SELECT *
               FROM products
               WHERE productID = :product_id';
     try {
-        $statement = $db->prepare($query);
-        $statement->bindValue(':product_id', $product_id);
-        $statement->execute();
-        $result = $statement->fetch();
-        $statement->closeCursor();
-        return $result;
+      $statement = $db->prepare($query);
+      $statement->bindValue(':product_id', $product_id);
+      $statement->execute();
+      $result = $statement->fetch();
+      $statement->closeCursor();
+      return $result;
     } catch (PDOException $e) {
-        $error_message = $e->getMessage();
-        display_db_error($error_message);
+      $error_message = $e->getMessage();
+      Database::display_db_error($error_message);
     }
-}
+  }
 
-function add_product($category_id, $code, $name, $description,
-        $price, $discount_percent) {
-    global $db;
+  function add_product(
+    $category_id,
+    $code,
+    $name,
+    $description,
+    $price,
+    $discount_percent
+  ) {
+    $db = Database::getDB();
     $query = 'INSERT INTO products
                  (categoryID, productCode, productName, description,
                   listPrice, discountPercent, dateAdded)
@@ -45,28 +55,35 @@ function add_product($category_id, $code, $name, $description,
                  (:category_id, :code, :name, :description, :price,
                   :discount_percent, NOW())';
     try {
-        $statement = $db->prepare($query);
-        $statement->bindValue(':category_id', $category_id);
-        $statement->bindValue(':code', $code);
-        $statement->bindValue(':name', $name);
-        $statement->bindValue(':description', $description);
-        $statement->bindValue(':price', $price);
-        $statement->bindValue(':discount_percent', $discount_percent);
-        $statement->execute();
-        $statement->closeCursor();
+      $statement = $db->prepare($query);
+      $statement->bindValue(':category_id', $category_id);
+      $statement->bindValue(':code', $code);
+      $statement->bindValue(':name', $name);
+      $statement->bindValue(':description', $description);
+      $statement->bindValue(':price', $price);
+      $statement->bindValue(':discount_percent', $discount_percent);
+      $statement->execute();
+      $statement->closeCursor();
 
-        // Get the last product ID that was automatically generated
-        $product_id = $db->lastInsertId();
-        return $product_id;
+      // Get the last product ID that was automatically generated
+      $product_id = $db->lastInsertId();
+      return $product_id;
     } catch (PDOException $e) {
-        $error_message = $e->getMessage();
-        display_db_error($error_message);
+      $error_message = $e->getMessage();
+      Database::display_db_error($error_message);
     }
-}
+  }
 
-function update_product($product_id, $code, $name, $description,
-                        $price, $discount_percent, $category_id) {
-    global $db;
+  function update_product(
+    $product_id,
+    $code,
+    $name,
+    $description,
+    $price,
+    $discount_percent,
+    $category_id
+  ) {
+    $db = Database::getDB();
     $query = 'UPDATE Products
               SET productName = :name,
                   productCode = :code,
@@ -76,35 +93,48 @@ function update_product($product_id, $code, $name, $description,
                   categoryID = :category_id
               WHERE productID = :product_id';
     try {
-        $statement = $db->prepare($query);
-        $statement->bindValue(':name', $name);
-        $statement->bindValue(':code', $code);
-        $statement->bindValue(':description', $description);
-        $statement->bindValue(':price', $price);
-        $statement->bindValue(':discount_percent', $discount_percent);
-        $statement->bindValue(':category_id', $category_id);
-        $statement->bindValue(':product_id', $product_id);
-        $row_count = $statement->execute();
-        $statement->closeCursor();
-        return $row_count;
+      $statement = $db->prepare($query);
+      $statement->bindValue(':name', $name);
+      $statement->bindValue(':code', $code);
+      $statement->bindValue(':description', $description);
+      $statement->bindValue(':price', $price);
+      $statement->bindValue(':discount_percent', $discount_percent);
+      $statement->bindValue(':category_id', $category_id);
+      $statement->bindValue(':product_id', $product_id);
+      $row_count = $statement->execute();
+      $statement->closeCursor();
+      return $row_count;
     } catch (PDOException $e) {
-        $error_message = $e->getMessage();
-        display_db_error($error_message);
+      $error_message = $e->getMessage();
+      Database::display_db_error($error_message);
     }
-}
+  }
 
-function delete_product($product_id) {
-    global $db;
+  function delete_product($product_id)
+  {
+    $db = Database::getDB();
     $query = 'DELETE FROM products WHERE productID = :product_id';
     try {
-        $statement = $db->prepare($query);
-        $statement->bindValue(':product_id', $product_id);
-        $row_count = $statement->execute();
-        $statement->closeCursor();
-        return $row_count;
+      $statement = $db->prepare($query);
+      $statement->bindValue(':product_id', $product_id);
+      $row_count = $statement->execute();
+      $statement->closeCursor();
+      return $row_count;
     } catch (PDOException $e) {
-        $error_message = $e->getMessage();
-        display_db_error($error_message);
+      $error_message = $e->getMessage();
+      Database::display_db_error($error_message);
     }
+  }
+  function get_product_id_by_name($product_name) {
+    $db = Database::getDB();
+    $query = "SELECT productID FROM products WHERE productName = '$product_name'";
+    try {
+      $stmt = $db -> query($query);
+      $result = $stmt ->fetch();
+      return $result['productID'];
+    } catch (PDOException $e) {
+      $error_message = $e ->getMessage();
+      Database::display_db_error($error_message);
+    }
+  }
 }
-?>
