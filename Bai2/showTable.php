7861b49172d11d6e9c4909defdd6e2ebc32e702e
Stork Nguyen <storknguyen.286@gmail.com>
2023-11-16 20:49:23 +0700

    update bai 2

---
+++ Bai2/showTable.php
@@ -0,0 +1,10 @@
+<?php 
+$op = $_GET['chon'];
+if($op!='') {
+  include('database.class.php');
+  $dao = new Dao("root","","udn");
+  $sql = "select * from {$op}";
+  $header = "DANH SACH {$op}";
+  $dao ->table($sql,$header);
+}
+?>
